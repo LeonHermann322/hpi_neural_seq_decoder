d06b4d2b7aa0bb46855f58b20447bcb6dcf17c81
tfiedlerdev <tfiedlerdevelopment@gmail.com>
2024-04-25 12:29:40 +0200

    Add setup script

---
+++ setup_lm_decoder_env.sh
@@ -0,0 +1,19 @@
+# Clone repository
+git clone https://github.com/fwillett/speechBCI.git
+
+# Install NeuralDecoder
+cd speechBCI/NeuralDecoder
+python -m pip install -e .
+
+# Install LanguageModelDecoder
+cd ../LanguageModelDecoder/srilm-1.7.3
+export SRILM=$PWD
+make -j8 MAKE_PIC=yes World
+make -j8 cleanest
+cd ../runtime/server/x86
+mkdir build
+cd build
+cmake ..
+make -j8
+cd ..
+python setup.py install
